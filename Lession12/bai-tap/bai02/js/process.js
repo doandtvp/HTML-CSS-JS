ccefb1098d86d8efb282e0e43d45d4f20f936c21
doantran119 <doandinhtran119@gmail.com>
2019-01-23 17:32:28 +0700

    Lession12-bai02 JS

---
+++ Lession12/bai-tap/bai02/js/process.js
@@ -0,0 +1,21 @@
+$(function() {
+
+	var element = document.getElementById('myP');
+
+	$('#deFault').click(function() {
+		document.getElementById('myP').style.fontSize = "100%";
+	});
+
+	$('#fsUp').click(function() {
+		var property = window.getComputedStyle(element, null).getPropertyValue("font-size");
+		var x = parseInt(property);
+		$('#myP').css('font-size' , x * 1.4);
+	});
+
+	$('#fsDown').click(function() {
+		var property = window.getComputedStyle(element, null).getPropertyValue("font-size");
+		var x = parseInt(property);
+		$('#myP').css('font-size' , x * 0.6);
+	});
+
+});
